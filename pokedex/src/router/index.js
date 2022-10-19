33f9a8d3ccc77788b618706c76eadb2f6fe809e1
Jaume <malet.jaume@gmail.com>
2022-10-20 00:24:41 +0200

    Canviar ../ per @/

--- pokedex/src/router/index.js
+++ pokedex/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import IndexView from '../views/IndexView.vue'
-import CombatView from '../views/CombatView.vue'
+import IndexView from '@/views/IndexView.vue'
+import CombatView from '@/views/CombatView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
